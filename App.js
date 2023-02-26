dd12119afbd0ae6b082a8a144f94c5af800e7edb
whiskacz <pawelblasiak1985@gmail.com>
2023-02-26 16:37:43 +0100

    React practice 2

--- App.js
+++ App.js
@@ -1,23 +1,31 @@
 class App extends React.Component {
+    state = {
+        value:""
+    }
 
-state = {
-    text: ""
-}
-handleClick = () => {
-    const letter = "pawko"
-    this.setState({
-        text : this.state.text + letter
-    })
+    handleInputChange = (e) => {
+        console.log(e.target.value)
+        this.setState({
+            value: e.target.value
+        })
+    }
+    resetButton = () => {
+        this.setState({
+            value: ""
+        })
+    }
 
-}
-render() {
-    return(
-        <React.Fragment>
-            <button onClick={this.handleClick}>Dodaj "A"</button>
-            <h1>{this.state.text}</h1>
-        </React.Fragment>
-    )
+    render(){
+        return(
+            <React.Fragment>
+                <input value={this.state.value} placeholder="wpisz" onChange={this.handleInputChange} type="text" />
+                <button onClick = {this.resetButton}>Reset</button>
+                <h1 className="title">{this.state.value}</h1>
+            </React.Fragment>
+
+
+        )
+    }    
 }
 
-}
 ReactDOM.render(<App/>, document.getElementById("root"))
